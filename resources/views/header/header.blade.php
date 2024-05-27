908818c1db1b86a6fe850d1b3c796f5ed23109aa
givenpenguin <99744458+givenpenguin@users.noreply.github.com>
2024-05-27 15:35:20 +0300

    1. Работа с view, компонентный подход

---
+++ resources/views/header/header.blade.php
@@ -0,0 +1,17 @@
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                {{ $title }}
+            </div>
+        </div>
+    </div>
+</header>
+
+@push('styles')
+    <style>
+        header {
+            background-color: #88a8ff;
+        }
+    </style>
+@endpush
---
+++ resources/views/pages/about/header.blade.php
@@ -0,0 +1,6 @@
+<div class="row">
+    <div class="col-12">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor iusto non veniam? Explicabo in maxime possimus ut velit? Commodi doloribus id laborum officia perferendis soluta tenetur ullam? Consectetur dolores, vel?</p>
+        <img src="" alt="">
+    </div>
+</div>
