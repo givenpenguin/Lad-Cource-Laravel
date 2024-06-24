0d2fe291d3a078d3ed9877f28547c9384bd0abaa
givenpenguin <99744458+givenpenguin@users.noreply.github.com>
2024-06-24 16:49:09 +0300

    1. Запросы

--- routes/web.php
+++ routes/web.php
@@ -9,30 +9,98 @@ use App\Http\Middleware\CheckValue;
 use App\Http\Middleware\EnsureTokenIsValid;
 use App\Http\Middleware\IsAdmin;
 use App\Http\Middleware\TestMiddleware;
+use App\Models\Post;
 use App\Models\User;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+
+    $post = Post::query()->withTrashed()->find(1);
+    if ($post->trashed()) {
+        $post->restore();
+    };
+
+    $post->forceDelete();
+
+//    $array = [
+//        'title' => 'title4',
+//        'text' => 'text4',
+//        'bool' => 1,
+//        'published_at' => now(),
+//    ];
+//    Post::create($array);
+//
+//    $post = Post::query()->updateOrCreate(
+//        ['title' => 'Пост 4', 'text' => 'Текст поста 4'],
+//        ['published_at' => now()],
+//    );
+//
+//    $post->text = 'new текст';
+//    dump($post->getOriginal('text'));
+
+
+//    $post_title = 'Пост 3';
+//    $post = Post::firstOrNew(
+//        [
+//            'title' => $post_title,
+//            'text' => 'Текст поста 1',
+//            'bool' => '1',
+//        ]
+//    );
+//    $post->save();
+//    dump($post);
+//
+//    $post = Post::query()->firstOrCreate(
+//        ['title' => $post_title, 'bool' => 1],
+//        ['text' => 'Новый текст']
+//    );
+//    $post->isDirty('text');
+//    $post->isClean();
+//
+//    $active_users_count = User::query()
+//        ->where('is_active', true)
+//        ->count();
+
+
+
+//    User::query()->where('is_active', false)->firstOr(function (){
+//        dump('Все пользователи активны');
+//    });
+//
+//    User::query()->findOr($id, function (){
+//        dump('Пользователь с id 1 не найден');
+//    });
+
 //    $users = User::query()->with('posts')->get();
 //    return view('home', ['users' => $users]);
 
-    $email1 = DB::table('users')->select('email')->where('id', 1)->first(); //stdObject
-    $email2 = User::query()->select('email')->where('id', 1)->first();
-    $email3 = User::query()->where('id', 1)->value('email');
-    dump($email1);
-    dump($email2);
-    dump($email3);
-
-    $emails1 = User::query()->pluck('email');
-    dump($emails1);
-
-    $emails2 = User::query()->chunk(2, function (Collection $collection) {
-        foreach ($collection as $item) {
-            //Отправить сообщение
-        }
-    });
+//    $email1 = DB::table('users')->select('email')->where('id', 1)->first(); //stdObject
+//    $email2 = User::query()->select('email')->where('id', 1)->first();
+//    $email3 = User::query()->where('id', 1)->value('email');
+//    dump($email1);
+//    dump($email2);
+//    dump($email3);
+//
+//    $emails1 = User::query()->pluck('email');
+//    dump($emails1);
+//
+//    User::query()->chunk(1000, function (Collection $collection) {
+//        foreach ($collection as $item) {
+//            //Отправить сообщение
+//        }
+//    });
+//
+//    User::query()->where('is_active', true)->chunkById(1000, function (Collection $collection) {
+//        foreach ($collection as $item) {
+//            $item->is_active = false;
+//        }
+//    }, 'created_at');
+//
+//    User::query()->lazy(100)->each(function ($user) {
+//
+//    });
 
 //    Auth::login($user);
 //    \Illuminate\Support\Facades\Session::put('status', 'active');
